d64e23dfa812da5f58381ed8b219d4612f71ac78
Chermann-KING <codes.smiling@gmail.com>
2023-08-27 19:11:19 +0200

    ♻️ HOME: Main > add <Categories />

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import { styled } from "styled-components";
 import { theme } from "../../theme/theme";
 import Header from "../../components/header/Header";
 import Hero from "./main/Hero";
-import { IoIosArrowForward } from "react-icons/io";
+import Categories from "./main/Categories";
 
 export default function () {
   return (
@@ -10,55 +10,7 @@ export default function () {
       <Header />
       <main>
         <Hero />
-        <section className="categories">
-          <div className="category-item">
-            <picture>
-              <img
-                src="/assets/shared/desktop/image-category-thumbnail-headphones.png"
-                alt="image-category-thumbnail-headphones"
-              />
-            </picture>
-            <h6>Headphones</h6>
-            <button>
-              <span className="button-label">Shop</span>
-              <span className="button-icon">
-                <IoIosArrowForward />
-              </span>
-            </button>
-          </div>
-
-          <div className="category-item">
-            <picture>
-              <img
-                src="/assets/shared/desktop/image-category-thumbnail-speakers.png"
-                alt="image-category-thumbnail-speakers"
-              />
-            </picture>
-            <h6>Speakers</h6>
-            <button>
-              <span className="button-label">Shop</span>
-              <span className="button-icon">
-                <IoIosArrowForward />
-              </span>
-            </button>
-          </div>
-
-          <div className="category-item">
-            <picture>
-              <img
-                src="/assets/shared/desktop/image-category-thumbnail-earphones.png"
-                alt="image-category-thumbnail-earphones"
-              />
-            </picture>
-            <h6>Earphones</h6>
-            <button>
-              <span className="button-label">Shop</span>
-              <span className="button-icon">
-                <IoIosArrowForward />
-              </span>
-            </button>
-          </div>
-        </section>
+        <Categories />
       </main>
     </HomeStyled>
   );
@@ -69,137 +21,5 @@ const HomeStyled = styled.div`
 
   main {
     margin: 0 auto;
-
-    .categories {
-      background-color: ${theme.colors.quaternaryColor};
-      width: 1110px;
-      height: 284px;
-
-      margin: 120px auto;
-
-      display: flex;
-      align-items: end;
-      justify-content: space-between;
-
-      .category-item {
-        width: 350px;
-        height: 204px;
-        background-color: ${theme.colors.tertiaryColor};
-        border-radius: 8px;
-        padding-bottom: 30px;
-
-        display: flex;
-        flex-direction: column;
-        justify-content: end;
-        align-items: center;
-        gap: 15px;
-
-        position: relative;
-
-        picture {
-          width: 176px;
-
-          position: absolute;
-          top: -30%;
-          left: 50%;
-          transform: translateX(-50%);
-
-          img {
-            max-width: 100%;
-            height: auto;
-          }
-        }
-
-        h6 {
-          font-size: ${theme.fonts.size.H6};
-        }
-
-        button {
-          cursor: pointer;
-
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          gap: 13px;
-
-          font-size: ${theme.fonts.size.SUBTITLE};
-          font-weight: ${theme.fonts.weights.bold};
-          letter-spacing: ${theme.fonts.charSpace.SubtilteCharSpace};
-          text-transform: uppercase;
-
-          border: none;
-          background-color: transparent;
-
-          .button-label {
-            color: ${theme.colors.blackColor};
-            opacity: 0.6;
-            /* &:hover {
-              color: ${theme.colors.primaryColor};
-              opacity: 1;
-            } */
-          }
-          .button-icon {
-            color: ${theme.colors.primaryColor};
-          }
-        }
-
-        &:hover {
-          cursor: pointer;
-          button .button-label {
-            color: ${theme.colors.primaryColor};
-            opacity: 1;
-          }
-        }
-      }
-    }
-    /* Responsive */
-    @media (max-width: 768px) {
-      // Tablet devices
-      .categories {
-        width: 689px;
-        height: 217px;
-        column-gap: 10px;
-
-        margin: 96px auto;
-
-        .category-item {
-          width: 223px;
-          height: 165px;
-
-          picture {
-            width: 103px;
-            top: -18%;
-          }
-        }
-      }
-    }
-
-    @media (max-width: 480px) {
-      // Mobile devices
-      .categories {
-        width: 327px;
-        height: 683px;
-        row-gap: 16px;
-
-        flex-direction: column;
-
-        margin-top: 40px;
-        margin-bottom: 120px;
-        margin-left: auto;
-        margin-right: auto;
-
-        .category-item {
-          width: 327px;
-          height: 165px;
-
-          margin-top: 30px;
-
-          picture {
-            width: 103px;
-            top: -18%;
-          }
-        }
-      }
-    }
   }
 `;
