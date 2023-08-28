5b92c50f9d24b54e536ebc68f4053fe06d71f210
Chermann-KING <codes.smiling@gmail.com>
2023-08-28 23:50:38 +0200

    ♻️ HOME: Main > add <About />

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Header from "../../components/header/Header";
 import Hero from "./main/Hero";
 import Categories from "./main/Categories";
 import Products from "./main/Products";
+import About from "./main/About";
 
 export default function () {
   return (
@@ -13,28 +14,7 @@ export default function () {
         <Hero />
         <Categories />
         <Products />
-        <section className="about">
-          <div className="about-infos">
-            <h2>
-              Bringing you the <span>best</span> audio gear
-            </h2>
-            <p>
-              Located at the heart of New York City, Audiophile is the premier
-              store for high end headphones, earphones, speakers, and audio
-              accessories. We have a large showroom and luxury demonstration
-              rooms available for you to browse and experience a wide range of
-              our products. Stop by our store to meet some of the fantastic
-              people who make Audiophile the best place to buy your portable
-              audio equipment.
-            </p>
-          </div>
-
-          <div
-            className="about-image"
-            role="img"
-            aria-label="best-gear-image"
-          ></div>
-        </section>
+        <About />
       </main>
     </HomeStyled>
   );
@@ -45,106 +25,5 @@ const HomeStyled = styled.div`
 
   main {
     margin: 0 auto;
-
-    .about {
-      margin: 200px auto;
-      width: 1110px;
-      height: 588px;
-
-      display: flex;
-      align-items: center;
-      column-gap: 125px;
-
-      .about-infos {
-        width: 445px;
-        height: 245px;
-
-        display: flex;
-        flex-direction: column;
-        row-gap: 32px;
-
-        color: ${theme.colors.secondaryColor};
-
-        h2 {
-          font-size: ${theme.fonts.size.H2};
-          font-weight: ${theme.fonts.weights.bold};
-          line-height: ${theme.fonts.lineSpace.H2LineSpace};
-          letter-spacing: ${theme.fonts.charSpace.H2CharSpace};
-          text-transform: uppercase;
-
-          span {
-            color: ${theme.colors.primaryColor};
-          }
-        }
-        p {
-          font-size: ${theme.fonts.size.SUBTITLE};
-          font-weight: ${theme.fonts.weights.medium};
-          line-height: ${theme.fonts.lineSpace.SubtitleLineSpace};
-          letter-spacing: ${theme.fonts.charSpace.SubtilteCharSpace};
-          opacity: 0.6;
-        }
-      }
-      .about-image {
-        width: 540px;
-        height: 100%;
-
-        border-radius: 10px;
-
-        background-image: url("/assets/shared/desktop/image-best-gear.jpg");
-        background-repeat: no-repeat;
-      }
-    }
-  }
-
-  @media (max-width: 768px) {
-    main {
-      .about {
-        margin: 96px auto;
-        width: 689px;
-        height: 633px;
-
-        flex-direction: column-reverse;
-        justify-content: space-between;
-        text-align: center;
-
-        .about-image {
-          width: 689px;
-          height: 300px;
-
-          background-image: url("/assets/shared/tablet/image-best-gear.jpg");
-          background-size: contain;
-        }
-
-        .about-infos {
-          width: 573px;
-          height: 270px;
-        }
-      }
-    }
-  }
-  @media (max-width: 480px) {
-    main {
-      .about {
-        margin: 120px auto;
-        width: 327px;
-        height: 698px;
-
-        .about-image {
-          width: 100%;
-          height: 300px;
-
-          background-image: url("/assets/shared/mobile/image-best-gear.jpg");
-        }
-        .about-infos {
-          width: 100%;
-          height: 358px;
-          row-gap: 32px;
-
-          h2 {
-            font-size: 28px;
-          }
-        }
-      }
-    }
   }
 `;
