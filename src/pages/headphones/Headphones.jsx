7fb5a5d28dda5021ca9f9cb6e16d35a64022520a
Chermann-KING <codes.smiling@gmail.com>
2023-08-29 18:57:10 +0200

    ♻️ HEADPHONES: Add <TitlePage /> and <Product />

--- src/pages/headphones/Headphones.jsx
+++ src/pages/headphones/Headphones.jsx
@@ -1,33 +1,75 @@
 import { styled } from "styled-components";
 import { theme } from "../../theme/theme";
 import Header from "../../components/header/Header";
+import TitlePage from "../../components/page-title/TitlePage";
+
+import Product from "./products/Product";
 
 export default function Headphones() {
   return (
     <HeadphonesStyled>
       <Header />
-      <div className="tilte-page">
-        <h2>Headphones</h2>
-      </div>
+      <TitlePage label={"Headphones"} />
+      <main>
+        <Product
+          subtitle={"New product"}
+          title={"XX99 Mark II Headphones"}
+          description={
+            "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound."
+          }
+          labelButton={"See product"}
+          desktopImage="/assets/shared/desktop/image-xx99-mark-two-headphones.jpg"
+          tabletImage="/assets/shared/tablet/image-xx99-mark-two-headphones.jpg"
+          mobileImage="/assets/shared/mobile/image-xx99-mark-two-headphones.jpg"
+          imageAlt="XX99 Mark II Headphones"
+        />
+
+        <Product
+          className={"reverse"}
+          title={"XX99 Mark I Headphones"}
+          description={
+            "As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go."
+          }
+          labelButton={"See product"}
+          desktopImage={
+            "/assets/shared/desktop/image-xx99-mark-one-headphones.jpg"
+          }
+          tabletImage={
+            "/assets/shared/tablet/image-xx99-mark-one-headphones.jpg"
+          }
+          mobileImage={
+            "/assets/shared/mobile/image-xx99-mark-one-headphones.jpg"
+          }
+          imageAlt={"XX99 Mark One Headphones"}
+        />
+
+        <Product
+          className={"margin"}
+          title={"XX59 Headphones"}
+          description={
+            "Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move."
+          }
+          labelButton={"See product"}
+          desktopImage={"/assets/shared/desktop/image-xx59-headphones.jpg"}
+          tabletImage={"/assets/shared/tablet/image-xx59-headphones.jpg"}
+          mobileImage={"/assets/shared/mobile/image-xx59-headphones.jpg"}
+          imageAlt={"XX59 Headphones"}
+        />
+      </main>
     </HeadphonesStyled>
   );
 }
 
 const HeadphonesStyled = styled.div`
-  .tilte-page {
-    height: 238px;
-    color: ${theme.colors.tertiaryColor};
-    background-color: ${theme.colors.secondaryColor};
+  main {
+    margin: 0 13.5%; // to review
+    margin-top: 160px;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    h2 {
-      font-size: ${theme.fonts.size.H2};
-      font-weight: ${theme.fonts.weights.bold};
-      letter-spacing: ${theme.fonts.charSpace.H2CharSpace};
-      text-transform: uppercase;
+    .reverse {
+      flex-direction: row-reverse;
+    }
+    .margin {
+      margin-bottom: 0px;
     }
   }
 `;
