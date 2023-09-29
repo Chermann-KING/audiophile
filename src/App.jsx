6bf33b2db74001a233148969aa72980ec5f86691
Chermann-KING <codes.smiling@gmail.com>
2023-09-29 12:04:17 +0200

    ✅ EARPHONES: Add <Header />, <TitlePage />, <Product />, <Categories />, <About /> and <Footer />

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Headphones from "./pages/headphones/Headphones";
 import Speakers from "./pages/speakers/Speakers";
+import Earphones from "./pages/earphones/Earphones";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/headphones" element={<Headphones />} />
       <Route path="/speakers" element={<Speakers />} />
+      <Route path="/earphones" element={<Earphones />} />
     </Routes>
   );
 }
